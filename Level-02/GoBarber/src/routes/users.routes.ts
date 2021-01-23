68856bc8e22c11174a6a51d4180f19127e5ce19c
henrique2m <btdavintehenrique@gmail.com>
2021-01-22 23:01:56 -0200

    chore: :sparkles: upload images

--- Level-02/GoBarber/src/routes/users.routes.ts
+++ Level-02/GoBarber/src/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -24,5 +28,28 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ erro: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      // delete user.password;
+      user.password = '';
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ erro: err.message });
+    }
+  },
+);
 
 export default usersRouter;
