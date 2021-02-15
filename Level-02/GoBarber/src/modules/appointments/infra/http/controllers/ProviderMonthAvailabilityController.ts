41f608d1f43ecf108d533322358933a144b1f7c8
henrique2m <btdavintehenrique@gmail.com>
2021-02-14 23:08:16 -0200

    fix: :sparkles: API Adjustments

--- Level-02/GoBarber/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ Level-02/GoBarber/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -5,7 +5,7 @@ import ListProviderMonthAvailabilityService from '@modules/appointments/services
 export default class ProviderMonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { month, year } = request.body;
+    const { month, year } = request.query;
 
     const listProvidersMonthAvailabilityController = container.resolve(
       ListProviderMonthAvailabilityService,
@@ -14,8 +14,9 @@ export default class ProviderMonthAvailabilityController {
     const availability = await listProvidersMonthAvailabilityController.execute(
       {
         provider_id,
-        month,
-        year,
+
+        month: Number(month),
+        year: Number(year),
       },
     );
 
